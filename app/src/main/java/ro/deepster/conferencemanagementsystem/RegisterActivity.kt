63d86fd1c6f1415059cd4939f58bb8ae8a45b7d7
Deepst3r <craciunescu.denis12@gmail.com>
2019-03-25 21:47:25 +0200

    Add Firebase auth

--- app/src/main/java/ro/deepster/conferencemanagementsystem/RegisterActivity.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/RegisterActivity.kt
@@ -2,11 +2,16 @@ package ro.deepster.conferencemanagementsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance()
     }
 }
