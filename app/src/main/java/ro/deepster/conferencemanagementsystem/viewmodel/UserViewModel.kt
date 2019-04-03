e2960b3f4feec721871dbda6d3fb00cf53672fed
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-03 22:39:10 +0300

    Add user view model

---
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/viewmodel/UserViewModel.kt
@@ -0,0 +1,44 @@
+package ro.deepster.conferencemanagementsystem.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import ro.deepster.conferencemanagementsystem.model.User
+
+class UserViewModel : ViewModel() {
+    companion object {
+        val TAG = UserViewModel::class.java.simpleName
+    }
+
+    private val database = FirebaseFirestore.getInstance()
+    private val usersRef = database.collection("users")
+
+    val userSnapshot: MutableLiveData<QuerySnapshot> by lazy {
+        MutableLiveData<QuerySnapshot>()
+    }
+
+    init {
+        usersRef.addSnapshotListener(EventListener<QuerySnapshot> { snapshot, e ->
+            if (e != null) {
+                Log.w(TAG, "Listen failed: $e")
+                return@EventListener
+            }
+
+            val source = if (snapshot != null && snapshot.metadata.hasPendingWrites())
+                "Local"
+            else
+                "Server"
+
+            if (snapshot != null) userSnapshot.value = snapshot
+        })
+    }
+
+    fun writeNewUser(userId: String, username: String, email: String) {
+        val user = User(userId, username, email)
+        database.collection("users").document(username)
+            .set(user)
+    }
+}
