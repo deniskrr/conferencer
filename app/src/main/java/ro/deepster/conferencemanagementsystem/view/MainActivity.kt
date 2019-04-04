ad0c8f450ff722c44f8e3f850bfe07b2af9c7e8e
ColdCipri <colder.ciprian@gmail.com>
2019-04-04 13:00:33 +0300

    Check if user is logged in

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/MainActivity.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/MainActivity.kt
@@ -1,12 +1,24 @@
 package ro.deepster.conferencemanagementsystem.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import ro.deepster.conferencemanagementsystem.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize Firebase components
+        auth = FirebaseAuth.getInstance()
+        if (auth.currentUser == null){
+            val intent = Intent(baseContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
