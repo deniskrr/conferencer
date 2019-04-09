0f9d34414c8641886f58bebe9de7111e70262c1a
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-09 17:25:23 +0300

    Observe log-in

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/auth/LoginFragment.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/auth/LoginFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.login_fragment.*
 import ro.deepster.conferencemanagementsystem.R
 import ro.deepster.conferencemanagementsystem.viewmodel.AuthViewModel
@@ -34,6 +37,12 @@ class LoginFragment : Fragment() {
 
         viewModel = activity!!.run { ViewModelProviders.of(this).get(AuthViewModel::class.java) }
 
+        viewModel.currentUser.observe(this, Observer<FirebaseUser> { currentUser ->
+            if (currentUser != null) {
+                NavHostFragment.findNavController(this).navigate(R.id.action_register_to_main)
+            }
+        })
+
         button_login.setOnClickListener {
             val email = edittext_email_login.text.toString()
             val password = edittext_password_login.text.toString()
@@ -41,10 +50,7 @@ class LoginFragment : Fragment() {
             viewModel.loginUser(email, password)
         }
 
-        text_no_account_login.setOnClickListener {
-            val navController = findNavController(it)
-            navController.navigate(R.id.action_login_to_register)
-        }
+        text_no_account_login.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_login_to_register))
 
     }
 }
--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/auth/RegisterFragment.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/auth/RegisterFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.register_fragment.*
 import ro.deepster.conferencemanagementsystem.R
 import ro.deepster.conferencemanagementsystem.viewmodel.AuthViewModel
@@ -32,6 +35,12 @@ class RegisterFragment : Fragment() {
 
         viewModel = activity!!.run { ViewModelProviders.of(this).get(AuthViewModel::class.java) }
 
+        viewModel.currentUser.observe(this, Observer<FirebaseUser> { currentUser ->
+            if (currentUser != null) {
+                NavHostFragment.findNavController(this).navigate(R.id.action_register_to_main)
+            }
+        })
+
         button_register.setOnClickListener {
             val username = edittext_username_register.text.toString()
 
@@ -46,10 +55,7 @@ class RegisterFragment : Fragment() {
         }
 
 
-        text_account_register.setOnClickListener {
-            val navController = findNavController(it)
-            navController.navigate(R.id.action_register_to_login)
-        }
+        text_account_register.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_register_to_login))
 
 
     }
