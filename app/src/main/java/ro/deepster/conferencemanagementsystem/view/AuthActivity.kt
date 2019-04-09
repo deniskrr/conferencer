7dabef1a58a7785f32dd88ee6028d8cf73568d36
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-09 16:44:52 +0300

    Use nav controller instead of fragment transactions

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/AuthActivity.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/AuthActivity.kt
@@ -2,8 +2,8 @@ package ro.deepster.conferencemanagementsystem.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
 import ro.deepster.conferencemanagementsystem.R
-import ro.deepster.conferencemanagementsystem.view.ui.auth.RegisterFragment
 
 class AuthActivity : AppCompatActivity() {
 
@@ -11,11 +11,10 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.auth_activity)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, RegisterFragment.newInstance())
-                .commitNow()
-        }
+        val navController = findNavController(this, R.id.main_content)
+
+
+
     }
 
 }
