e98f2d5fdb234966d3a3150946ee8d72b007c3eb
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-11 20:32:57 +0300

    Create new conference fragment

---
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/main/NewConferenceFragment.kt
@@ -0,0 +1,33 @@
+package ro.deepster.conferencemanagementsystem.view.main
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import ro.deepster.conferencemanagementsystem.R
+
+class NewConferenceFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = NewConferenceFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_new_conference, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
