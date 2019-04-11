a5b3899b6a45462debac770d182202dd4bdf9611
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-11 19:46:30 +0300

    Update settings activity

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/SettingsActivity.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/SettingsActivity.kt
@@ -2,6 +2,7 @@ package ro.deepster.conferencemanagementsystem.view
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import ro.deepster.conferencemanagementsystem.R
 
@@ -11,12 +12,21 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        setTitle("Settings")
+        title = "Settings"
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.settings_menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_save_settings -> {
+                //TODO If the values entered are correct, then create a new User object and write it to the database
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
