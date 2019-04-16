4c5d117c5d5aa84d4cf11686d255460091d32c23
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-16 23:27:15 +0300

    Connect main fragment to new conference fragment

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/main/MainFragment.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/main/MainFragment.kt
@@ -6,9 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_fragment.*
 import ro.deepster.conferencemanagementsystem.R
+import ro.deepster.conferencemanagementsystem.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
+
 
     companion object {
         fun newInstance() = MainFragment()
@@ -20,13 +25,18 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val controller = findNavController(nav_host_fragment)
+
+        fab_new_conference.setOnClickListener {
+            controller.navigate(R.id.action_main_to_new)
+        }
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
