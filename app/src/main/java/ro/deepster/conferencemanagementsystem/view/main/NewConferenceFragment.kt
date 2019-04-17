ec5264d0ce3eba95377e70abf53cd0b68c01b861
Deepst3r <craciunescu.denis12@gmail.com>
2019-04-17 12:27:57 +0300

    Update NewConference layout

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/main/NewConferenceFragment.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/main/NewConferenceFragment.kt
@@ -8,8 +8,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.new_conference_fragment.*
 import ro.deepster.conferencemanagementsystem.R
+import ro.deepster.conferencemanagementsystem.model.Conference
+import ro.deepster.conferencemanagementsystem.model.ReviewerItem
 import ro.deepster.conferencemanagementsystem.viewmodel.MainViewModel
 
 class NewConferenceFragment : Fragment() {
@@ -35,6 +42,31 @@ class NewConferenceFragment : Fragment() {
             spinner_co_chair_new.setItems(viewModel.getUsers())
             spinner_reviewers_new.setItems(viewModel.getUsers())
         })
+
+        val adapter = GroupAdapter<ViewHolder>()
+        recycler_reviewers_new.adapter = adapter
+        recycler_reviewers_new.layoutManager = LinearLayoutManager(activity)
+
+        spinner_reviewers_new.setOnItemSelectedListener { view, position, id, item ->
+            adapter.add(ReviewerItem(item.toString()))
+        }
+
+        button_create_new.setOnClickListener {
+            val title = textlayout_title_new.editText?.text.toString()
+            val co_chair = spinner_co_chair_new.getItems<String>().get(spinner_co_chair_new.selectedIndex)
+
+            val reviewers = mutableListOf<String>()
+            for (i in 0..adapter.itemCount - 1) {
+                reviewers.add((adapter.getItem(i) as ReviewerItem).username)
+            }
+
+            val conference = Conference(title, co_chair, reviewers)
+
+            viewModel.addConference(conference)
+
+            val controller = NavHostFragment.findNavController(nav_host_fragment)
+            controller.navigateUp()
+        }
 
     }
 
