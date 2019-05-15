29523966051291bf1f63c11dbfda781ea2578cb5
Deepst3r <craciunescu.denis12@gmail.com>
2019-05-15 18:11:19 +0300

    Add RoleActivity + starting navigation

---
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/conference/RoleActivity.kt
@@ -0,0 +1,15 @@
+package ro.deepster.conferencemanagementsystem.view.conference
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ro.deepster.conferencemanagementsystem.R
+
+class RoleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_role_activity)
+    }
+
+
+}
---
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/viewmodel/RoleViewModel.kt
@@ -0,0 +1,7 @@
+package ro.deepster.conferencemanagementsystem.viewmodel
+
+import androidx.lifecycle.ViewModel;
+
+class RoleViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
