a1d4005cfdc764b84212fccb38785bc6a0075fef
Deepst3r <craciunescu.denis12@gmail.com>
2019-05-15 21:19:53 +0300

    Add optional parameters for every field.
    Necessary for empty constructor

--- app/src/main/java/ro/deepster/conferencemanagementsystem/model/Conference.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/model/Conference.kt
@@ -6,8 +6,8 @@ enum class Period {
 }
 
 class Conference(
-    val title: String,
-    val co_chair: String,
+    val title: String = "",
+    val co_chair: String = "",
     val reviewers: List<String> = mutableListOf(),
     val period: Period = Period.CALL_FOR_PAPERS
 )
