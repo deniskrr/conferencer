1b52a0904141cad6e1a2f773a6dbbf609cb2ec93
Deepst3r <craciunescu.denis12@gmail.com>
2019-05-15 21:21:19 +0300

    Add role activity + skeleton for starting RoleFragment based on the user's role

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/conference/RoleActivity.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/conference/RoleActivity.kt
@@ -1,14 +1,54 @@
 package ro.deepster.conferencemanagementsystem.view.conference
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import ro.deepster.conferencemanagementsystem.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import ro.deepster.conferencemanagementsystem.model.Conference
+import ro.deepster.conferencemanagementsystem.model.User
+import ro.deepster.conferencemanagementsystem.viewmodel.RoleViewModel
+
 
 class RoleActivity : AppCompatActivity() {
+    companion object {
+        val CONFERENCE_NAME = "Conference Name"
+        val TAG = RoleActivity::class.java.canonicalName
+    }
+
+    private lateinit var user: User
+    private lateinit var conference: Conference
+
+    //    lateinit var conference: Conference
+    private lateinit var viewModel: RoleViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_role_activity)
+        setContentView(ro.deepster.conferencemanagementsystem.R.layout.activity_role_activity)
+
+        // The activity will always be started by an Intent with Conference name extra
+        val conferenceName = intent.extras!!.getString(CONFERENCE_NAME)!!
+
+        viewModel = ViewModelProviders.of(this).get(RoleViewModel::class.java)
+
+        viewModel.setConference(conferenceName)
+
+        viewModel.currentConference.observe(this, Observer { newConference ->
+            conference = newConference
+            viewModel.currentUser.observe(this, Observer { newUser ->
+                user = newUser
+                if (user.username == "admin") {
+                    Log.d(TAG, "Starting admin")
+                    //TODO Start admin
+                } else if (user.username == conference.co_chair) {
+                    Log.d(TAG, "Starting co-chair")
+                    //TODO Start co-chair
+                } else if (user.username in conference.reviewers) {
+                    Log.d(TAG, "Starting reviewer")
+                    //TODO Start reviewers
+                }
+            })
+        })
     }
 
 
--- app/src/main/java/ro/deepster/conferencemanagementsystem/viewmodel/RoleViewModel.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/viewmodel/RoleViewModel.kt
@@ -1,7 +1,34 @@
 package ro.deepster.conferencemanagementsystem.viewmodel
 
-import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import ro.deepster.conferencemanagementsystem.model.Conference
+import ro.deepster.conferencemanagementsystem.model.User
 
 class RoleViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val currentConference: MutableLiveData<Conference> by lazy {
+        MutableLiveData<Conference>()
+    }.apply { Conference() }
+
+    val currentUser: MutableLiveData<User> by lazy {
+        MutableLiveData<User>()
+    }.apply { User() }
+
+    init {
+        FirebaseFirestore.getInstance().collection("users")
+            .whereEqualTo("uid", FirebaseAuth.getInstance().currentUser!!.uid)
+            .addSnapshotListener { documentSnapshot, _ ->
+                currentUser.value = documentSnapshot?.toObjects(User::class.java)?.get(0)
+            }
+    }
+
+    fun setConference(conferenceName: String) {
+        FirebaseFirestore.getInstance().collection("conferences").document(conferenceName)
+            .addSnapshotListener { documentSnapshot, _ ->
+                currentConference.value = documentSnapshot?.toObject(Conference::class.java)
+            }
+    }
 }
