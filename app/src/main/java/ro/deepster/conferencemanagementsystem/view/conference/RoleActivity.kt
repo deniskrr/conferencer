acc344fc0d81f61c12e0f45c3eb2a2628d2a17a6
Deepst3r <craciunescu.denis12@gmail.com>
2019-05-16 12:34:24 +0300

    Make navigation to AuthorFragment possible

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/conference/RoleActivity.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/conference/RoleActivity.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation.findNavController
+import ro.deepster.conferencemanagementsystem.R
 import ro.deepster.conferencemanagementsystem.model.Conference
 import ro.deepster.conferencemanagementsystem.model.User
 import ro.deepster.conferencemanagementsystem.viewmodel.RoleViewModel
@@ -24,8 +26,9 @@ class RoleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(ro.deepster.conferencemanagementsystem.R.layout.activity_role_activity)
+        setContentView(R.layout.activity_role_activity)
 
+        val controller = findNavController(this, R.id.role_content)
         // The activity will always be started by an Intent with Conference name extra
         val conferenceName = intent.extras!!.getString(CONFERENCE_NAME)!!
 
@@ -35,6 +38,9 @@ class RoleActivity : AppCompatActivity() {
 
         viewModel.currentConference.observe(this, Observer { newConference ->
             conference = newConference
+
+            // Show the conference title in the action bar
+            title = conference.title
             viewModel.currentUser.observe(this, Observer { newUser ->
                 user = newUser
                 if (user.username == "admin") {
@@ -46,6 +52,9 @@ class RoleActivity : AppCompatActivity() {
                 } else if (user.username in conference.reviewers) {
                     Log.d(TAG, "Starting reviewer")
                     //TODO Start reviewers
+                } else {
+                    Log.d(TAG, "Starting author")
+                    controller.navigate(R.id.action_generic_to_author)
                 }
             })
         })
