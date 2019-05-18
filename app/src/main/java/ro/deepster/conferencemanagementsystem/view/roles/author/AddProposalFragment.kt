c0d5db683d6fc9d59e64ce4bd39d9f57ebfbdf3e
Deepst3r <craciunescu.denis12@gmail.com>
2019-05-18 14:09:07 +0300

    Add addProposal fragment

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/roles/author/AddProposalFragment.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/roles/author/AddProposalFragment.kt
@@ -6,24 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_author.*
 import ro.deepster.conferencemanagementsystem.R
 
-class AuthorFragment : Fragment() {
+class AddProposalFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_author, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        button_new_proposal.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_author_to_new_proposal))
+        return inflater.inflate(R.layout.fragment_add_proposal, container, false)
     }
 
 
