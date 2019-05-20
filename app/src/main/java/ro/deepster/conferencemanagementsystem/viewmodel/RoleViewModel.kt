3b49e9041f12a581637092aa89c9c10db655a238
deepster <craciunescu.denis12@gmail.com>
2019-05-20 21:24:07 +0300

    Add Author functionality to RoleViewModel

--- app/src/main/java/ro/deepster/conferencemanagementsystem/viewmodel/RoleViewModel.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/viewmodel/RoleViewModel.kt
@@ -1,13 +1,25 @@
 package ro.deepster.conferencemanagementsystem.viewmodel
 
+import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageMetadata
 import ro.deepster.conferencemanagementsystem.model.Conference
+import ro.deepster.conferencemanagementsystem.model.Proposal
 import ro.deepster.conferencemanagementsystem.model.User
+import java.util.*
 
 class RoleViewModel : ViewModel() {
+
+    val storage: FirebaseStorage = FirebaseStorage.getInstance()
+
+    val paperLink : MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }.apply { "" }
 
     val currentConference: MutableLiveData<Conference> by lazy {
         MutableLiveData<Conference>()
@@ -30,5 +42,24 @@ class RoleViewModel : ViewModel() {
             .addSnapshotListener { documentSnapshot, _ ->
                 currentConference.value = documentSnapshot?.toObject(Conference::class.java)
             }
+    }
+
+    fun addProposal(proposal : Proposal) {
+        FirebaseFirestore.getInstance().collection("proposals").document(proposal.title).set(proposal)
+    }
+
+    fun uploadPaper(selectedPaper: Uri) {
+        val metadata = StorageMetadata.Builder()
+            .setContentType("application/pdf")
+            .build()
+
+        val reference = storage.getReference("/papers/${UUID.randomUUID()}")
+
+        reference.putFile(selectedPaper, metadata)
+                .addOnSuccessListener {
+                    reference.downloadUrl.addOnSuccessListener {
+                        paperLink.value = it.toString()
+                    }
+                }
     }
 }
