f7e2d34262550d811ee46fd3f4f5d56a26a4aa3a
deepster <craciunescu.denis12@gmail.com>
2019-05-21 12:50:37 +0300

    Make a proposal have only one author (the creator)

--- app/src/main/java/ro/deepster/conferencemanagementsystem/model/Proposal.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/model/Proposal.kt
@@ -1,11 +1,11 @@
 package ro.deepster.conferencemanagementsystem.model
 
 data class Proposal(
-    val title: String,
-    val topics : String,
-    val keywords : String,
-    val authors : List<String>,
-    val paperLink : String
+    val title: String = "",
+    val topics: String = "",
+    val keywords: String = "",
+    val author: String = "",
+    val paperLink: String = ""
 
 
 )
