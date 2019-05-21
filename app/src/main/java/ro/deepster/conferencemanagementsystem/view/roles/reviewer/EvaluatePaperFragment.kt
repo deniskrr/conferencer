07340987bd2c002742fb298d448dc6988dca4c25
deepster <craciunescu.denis12@gmail.com>
2019-05-21 22:03:18 +0300

    Add EvaluatePaper Fragment

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/roles/reviewer/EvaluatePaperFragment.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/roles/reviewer/EvaluatePaperFragment.kt
@@ -1,18 +1,16 @@
 package ro.deepster.conferencemanagementsystem.view.roles.reviewer
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.reviewer_fragment.*
 import ro.deepster.conferencemanagementsystem.R
 
 
-class ReviewerFragment : Fragment() {
-
+class EvaluatePaperFragment : Fragment() {
 
     private lateinit var viewModel: ReviewerViewModel
 
@@ -20,15 +18,18 @@ class ReviewerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(ro.deepster.conferencemanagementsystem.R.layout.reviewer_fragment, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_evaluate_paper, container, false)
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(activity!!).get(ReviewerViewModel::class.java)
 
-        button_evaluate_paper.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_reviewewr_to_evaluate_paper))
-        button_bid_proposal.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_reviewer_to_bid_proposal))
+
     }
 
 }
+
+
