6cf92df6536429b97b7b48b1d9f0393edab5a14c
deepster <craciunescu.denis12@gmail.com>
2019-05-22 00:03:36 +0300

    Add paper evaluation functionality

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/roles/reviewer/EvaluatePaperExtendedFragment.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/roles/reviewer/EvaluatePaperExtendedFragment.kt
@@ -7,6 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import kotlinx.android.synthetic.main.activity_role_activity.*
 import kotlinx.android.synthetic.main.fragment_evaluate_paper_extended.*
 import ro.deepster.conferencemanagementsystem.R
 import ro.deepster.conferencemanagementsystem.model.Proposal
@@ -15,6 +18,7 @@ import ro.deepster.conferencemanagementsystem.model.Proposal
 class EvaluatePaperExtendedFragment : Fragment() {
 
     private lateinit var proposal: Proposal
+    private lateinit var viewModel: ReviewerViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +30,7 @@ class EvaluatePaperExtendedFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(activity!!).get(ReviewerViewModel::class.java)
         proposal = arguments?.get(EvaluatePaperFragment.PROPOSAL) as Proposal
 
         text_paper.text = proposal.title
@@ -33,6 +38,16 @@ class EvaluatePaperExtendedFragment : Fragment() {
         button_see_paper.setOnClickListener {
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(proposal.paperLink))
             startActivity(browserIntent)
+        }
+
+        button_accept.setOnClickListener {
+            viewModel.acceptPaper(proposal)
+            findNavController(role_content).navigateUp()
+        }
+
+        button_reject.setOnClickListener {
+            viewModel.rejectPaper(proposal)
+            findNavController(role_content).navigateUp()
         }
     }
 
--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/roles/reviewer/ReviewerViewModel.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/roles/reviewer/ReviewerViewModel.kt
@@ -15,4 +15,22 @@ class ReviewerViewModel : RoleViewModel() {
         FirebaseFirestore.getInstance().collection("conferences").document(RoleActivity.conferenceName).set(conference)
     }
 
+    fun acceptPaper(proposal: Proposal) {
+        val user = currentUser.value!!.username
+        val conference = currentConference.value!!
+        conference.proposals.remove(proposal)
+        proposal.acceptProposal(user)
+        conference.proposals.add(proposal)
+        FirebaseFirestore.getInstance().collection("conferences").document(RoleActivity.conferenceName).set(conference)
+    }
+
+    fun rejectPaper(proposal: Proposal) {
+        val user = currentUser.value!!.username
+        val conference = currentConference.value!!
+        conference.proposals.remove(proposal)
+        proposal.rejectProposal(user)
+        conference.proposals.add(proposal)
+        FirebaseFirestore.getInstance().collection("conferences").document(RoleActivity.conferenceName).set(conference)
+    }
+
 }
