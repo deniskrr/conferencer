6ae617378c2a44751e71e857df1559da244b3763
deepster <craciunescu.denis12@gmail.com>
2019-05-21 22:17:31 +0300

    Show list of bidden papers in EvaluatePaperFragment

--- app/src/main/java/ro/deepster/conferencemanagementsystem/view/roles/reviewer/EvaluatePaperFragment.kt
+++ app/src/main/java/ro/deepster/conferencemanagementsystem/view/roles/reviewer/EvaluatePaperFragment.kt
@@ -6,12 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_role_activity.*
+import kotlinx.android.synthetic.main.fragment_evaluate_paper.*
 import ro.deepster.conferencemanagementsystem.R
+import ro.deepster.conferencemanagementsystem.model.Proposal
+import ro.deepster.conferencemanagementsystem.model.ProposalItem
 
 
 class EvaluatePaperFragment : Fragment() {
 
+    val adapter = GroupAdapter<ViewHolder>()
     private lateinit var viewModel: ReviewerViewModel
 
     override fun onCreateView(
@@ -27,7 +37,28 @@ class EvaluatePaperFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(activity!!).get(ReviewerViewModel::class.java)
 
+        adapter.setOnItemClickListener { item, view ->
+            if (item is ProposalItem) {
+                val controller = NavHostFragment.findNavController(role_content)
+                controller.navigate(R.id.action_evaluate_extended)
+            }
+        }
 
+        recycler_bidden_proposals.adapter = adapter
+        recycler_bidden_proposals.layoutManager = LinearLayoutManager(activity)
+
+        viewModel.currentConference.observe(this, Observer { currentConference ->
+            adapter.clear()
+
+            val currentUser = viewModel.currentUser.value!!.username
+
+            adapter.addAll(
+                currentConference.proposals.filter { proposal: Proposal ->
+                    // Show proposals that you bid on
+                    currentUser in proposal.bidders
+                }.map { proposal -> ProposalItem(proposal) }
+            )
+        })
     }
 
 }
